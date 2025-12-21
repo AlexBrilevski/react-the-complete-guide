332aed2a91fdbbdbb7ed08a5d5a4efdceac6e3aa
Alex Brilevski <brilevski.alex@gmail.com>
2025-12-21 13:36:03 +0300

    Link to the products page was added to the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
-  return <h1>The Home page</h1>;
+  return (
+  <>
+  <h1>The Home page</h1>
+  <p>Go to <Link to="/products">the list of products</Link>.</p>
+  </>
+);
 };
 
 export default Home;
