5fb789bcbe315bfbd6803924a0422099c8bf4159
Alex Brilevski <brilevski.alex@gmail.com>
2025-12-21 13:18:03 +0300

    Basic components and routing for them were added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { createRoutesFromElements, Route, createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+
+const routerDefinitions = createRoutesFromElements(
+  <Route>
+    <Route path='/' element={<Home />} />
+    <Route path='/products' element={<Products />} />
+  </Route>
+);
+
+const router = createBrowserRouter(routerDefinitions);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
