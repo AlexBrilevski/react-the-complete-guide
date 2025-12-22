f5c71f368de44cf4df6fe032c1b7f04e938c5a0e
Alex Brilevski <brilevski.alex@gmail.com>
2025-12-22 11:32:31 +0300

    Links to product details pages were added to the products listing page

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,5 +1,26 @@
+import { Link } from "react-router-dom";
+
+const PRODUCTS_DATA = [
+  { id: 'p1', name: 'Product 1' },
+  { id: 'p2', name: 'Product 2' },
+  { id: 'p3', name: 'Product 3' },
+];
+
 const Products = () => {
-  return <h1>The Products page</h1>;
+  return (
+    <>
+      <h1>The Products page</h1>
+      <ul>
+        {PRODUCTS_DATA.map(product => (
+          <li key={product.id}>
+            <Link to={`/products/${product.id}`}>
+              {product.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default Products;
