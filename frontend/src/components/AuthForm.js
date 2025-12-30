979b8ddcde99cf031582c4fee8303334ac9bc2a3
Alex Brilevski <brilevski.alex@gmail.com>
2025-12-30 17:22:16 +0300

    Updating the auth form state on submit was added

--- frontend/src/components/AuthForm.js
+++ frontend/src/components/AuthForm.js
@@ -1,11 +1,13 @@
-import { useActionData, useSearchParams, Form, Link } from 'react-router-dom';
+import { useActionData, useNavigation, useSearchParams, Form, Link } from 'react-router-dom';
 
 import classes from './AuthForm.module.css';
 
 function AuthForm() {
   const data = useActionData();
+  const navigation = useNavigation();
   const [searchParams] = useSearchParams();
   const isLogin = searchParams.get('mode') === 'login';
+  const isSubmitting = navigation.state === 'submitting';
 
   return (
     <>
@@ -29,7 +31,9 @@ function AuthForm() {
           <Link to={`?mode=${isLogin ? 'signup' : 'login'}`}>
             {isLogin ? 'Create new user' : 'Login'}
           </Link>
-          <button>Save</button>
+          <button disabled={isSubmitting}>
+            {isSubmitting ? 'Submitting...' : 'Save'}
+          </button>
         </div>
       </Form>
     </>
