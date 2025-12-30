d49e1c2749c0250d47a4cc2c2bfffc0b1875e780
Alex Brilevski <brilevski.alex@gmail.com>
2025-12-30 17:29:26 +0300

    Automatic logout on timeout was added

--- frontend/src/pages/RootLayout.jsx
+++ frontend/src/pages/RootLayout.jsx
@@ -1,8 +1,21 @@
-import { Outlet, useNavigation } from "react-router-dom";
+import { Outlet, useLoaderData, useSubmit, useNavigation } from "react-router-dom";
 import MainNavigation from "../components/MainNavigation";
+import { useEffect } from "react";
 
 const RootLayout = () => {
+  const token = useLoaderData();
+  const submit = useSubmit();
   const navigation = useNavigation();
+
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    setTimeout(() => {
+      submit(null, { action: '/auth', method: 'POST' });
+    }, 1 * 60 * 60 * 1000);
+  }, [token]);
 
   return (
     <>
