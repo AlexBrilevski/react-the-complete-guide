bb89614001fccbaf5acab914111f465c039a1b79
Alex Brilevski <brilevski.alex@gmail.com>
2025-12-31 11:10:04 +0300

    A missing dependency was added to useEffect hook

--- frontend/src/pages/RootLayout.jsx
+++ frontend/src/pages/RootLayout.jsx
@@ -24,7 +24,7 @@ const RootLayout = () => {
     setTimeout(() => {
       submit(null, { action: '/auth', method: 'POST' });
     }, tokenDuration);
-  }, [token]);
+  }, [token, submit]);
 
   return (
     <>
