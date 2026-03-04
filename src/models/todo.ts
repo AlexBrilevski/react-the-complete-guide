2c7e0567ef0976d1f441cceff27fc053d16deb45
Alex Brilevski <brilevski.alex@gmail.com>
2026-03-04 20:18:01 +0300

    Todo data model was added. Outputting a todo was refactored

---
+++ src/models/todo.ts
@@ -0,0 +1,11 @@
+class Todo {
+  id: string;
+  text: string;
+
+  constructor(todoText: string) {
+    this.id = new Date().toISOString() + Math.random() * 10;
+    this.text = todoText;
+  }
+}
+
+export default Todo;
