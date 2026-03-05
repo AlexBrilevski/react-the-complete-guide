556a935b4075adec9edd7cc425cadf7310dfdeb3
Alex Brilevski <brilevski.alex@gmail.com>
2026-03-05 21:40:44 +0300

    Logic of adding a new To Do to the app state was implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
+import { useState } from "react";
+
 import Todo from "./models/todo";
 import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
 
-const todos = [
-  new Todo('Learn React'),
-  new Todo('Learn TypeScript'),
-];
-
 function App() {
-  function addTodo(todoText: string) { }
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  function addTodo(todoText: string) {
+    setTodos(prevTodos => prevTodos.concat(new Todo(todoText)));
+  }
 
   return (
     <div>
