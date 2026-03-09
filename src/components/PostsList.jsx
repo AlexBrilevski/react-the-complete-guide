8dd3423d973b07d9f78f6fa30846094b092eb147
Alex Brilevski <brilevski.alex@gmail.com>
2026-03-09 21:48:00 +0300

    Rendering the new post modal conditionally was implemented

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -6,8 +6,13 @@ import Post from "./Post";
 import classes from './PostsList.module.css';
 
 function PostsList() {
+  const [isModalVisible, setIsModalVisible] = useState(true);
   const [enteredBody, setEnteredBody] = useState('Author 1');
   const [enteredAuthor, setEnteredAuthor] = useState('Post text 1');
+
+  function hideModalHandler() {
+    setIsModalVisible(false);
+  }
 
   function changeBodyHandler(event) {
     setEnteredBody(event.target.value);
@@ -19,12 +24,14 @@ function PostsList() {
 
   return (
     <>
-      <Modal>
-        <NewPost
-          onChangeBody={changeBodyHandler}
-          onChangeAuthor={changeAuthorHandler}
-        />
-      </Modal>
+      {isModalVisible && (
+        <Modal onClose={hideModalHandler}>
+          <NewPost
+            onChangeBody={changeBodyHandler}
+            onChangeAuthor={changeAuthorHandler}
+          />
+        </Modal>
+      )}
       <ul className={classes.posts}>
         <Post author={enteredAuthor} body={enteredBody} />
         <Post author='Author 2' body='Post text 2' />
