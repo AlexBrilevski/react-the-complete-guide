6dc88977f1210da06cb2cb16b4243d543fb91a81
Alex Brilevski <brilevski.alex@gmail.com>
2026-03-09 20:15:48 +0300

    Basic New Post component was added

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,12 +1,16 @@
+import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from './PostsList.module.css';
 
 function PostsList() {
   return (
-    <ul className={classes.posts}>
-      <Post author='Author 1' body='Post text 1' />
-      <Post author='Author 2' body='Post text 2' />
-    </ul>
+    <>
+      <NewPost />
+      <ul className={classes.posts}>
+        <Post author='Author 1' body='Post text 1' />
+        <Post author='Author 2' body='Post text 2' />
+      </ul>
+    </>
   );
 }
 
