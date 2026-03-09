1a5c369172a382079483334ac14ee00e76c38661
Alex Brilevski <brilevski.alex@gmail.com>
2026-03-09 21:14:02 +0300

    Logic for updating the first post state via the new post form was implemented

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,15 +1,25 @@
 import classes from './NewPost.module.css';
 
-function NewPost() {
+function NewPost(props) {
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} />
+        <textarea
+          id="body"
+          rows={3}
+          required
+          onChange={props.onChangeBody}
+        />
       </p>
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required />
+        <input
+          id="name"
+          type="text"
+          required
+          onChange={props.onChangeAuthor}
+        />
       </p>
     </form>
   );
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,13 +1,28 @@
+import { useState } from "react";
 import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from './PostsList.module.css';
 
 function PostsList() {
+  const [enteredBody, setEnteredBody] = useState('Author 1');
+  const [enteredAuthor, setEnteredAuthor] = useState('Post text 1');
+
+  function changeBodyHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
+  function changeAuthorHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
   return (
     <>
-      <NewPost />
+      <NewPost
+        onChangeBody={changeBodyHandler}
+        onChangeAuthor={changeAuthorHandler}
+      />
       <ul className={classes.posts}>
-        <Post author='Author 1' body='Post text 1' />
+        <Post author={enteredAuthor} body={enteredBody} />
         <Post author='Author 2' body='Post text 2' />
       </ul>
     </>
