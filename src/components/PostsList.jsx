1fceb611d29872e52e731585e1f26eef88218f6d
Alex Brilevski <brilevski.alex@gmail.com>
2026-03-11 23:48:50 +0300

    Logic of adding a new post to the state of the post list component was added

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,14 +1,23 @@
+import { useState } from "react";
+
 import Modal from "./Modal";
 import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from './PostsList.module.css';
 
 function PostsList({ isPosting, onStopPosting }) {
+  const [posts, setPosts] = useState([]);
+
+  function addPostHandler(newPost) {
+    setPosts(existingPosts => [newPost, ...existingPosts]);
+  }
+
   return (
     <>
       {isPosting && (
         <Modal onClose={onStopPosting}>
           <NewPost
+            onAddPost={addPostHandler}
             onCancel={onStopPosting}
           />
         </Modal>
