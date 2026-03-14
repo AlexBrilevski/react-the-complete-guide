67652ec611a3a37d42588ec25ab72a75b6607874
Alex Brilevski <brilevski.alex@gmail.com>
2026-03-14 09:33:51 +0300

    Routes were added to the app. Initial refactoring of components used as route layouts

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,11 +1,8 @@
 import { useState, useEffect } from "react";
-
-import Modal from "./Modal";
-import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from './PostsList.module.css';
 
-function PostsList({ isPosting, onStopPosting }) {
+function PostsList() {
   const [posts, setPosts] = useState([]);
   const [isFetching, setIsFetching] = useState(false);
 
@@ -34,14 +31,6 @@ function PostsList({ isPosting, onStopPosting }) {
 
   return (
     <>
-      {isPosting && (
-        <Modal onClose={onStopPosting}>
-          <NewPost
-            onAddPost={addPostHandler}
-            onCancel={onStopPosting}
-          />
-        </Modal>
-      )}
       {!isFetching && posts.length > 0 && (
         <ul className={classes.posts}>
           {posts.map((post, index) => {
