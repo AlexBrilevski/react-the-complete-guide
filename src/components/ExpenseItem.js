7c0ac3398e44ee823fc55c3f65fb8e0a42314085
Alex Brilevski <brilevski.alex@gmail.com>
2026-03-22 12:51:43 +0300

    The expense date formatting was added

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,18 @@
 import './ExpenseItem.css';
 
 function ExpenseItem({ date, title, amount }) {
+  const formattedDate = {
+    month: date.toLocaleString('en-US', { month: 'long' }),
+    day: date.toLocaleString('en-US', { day: '2-digit' }),
+    year: date.getFullYear(),
+  };
+
   return (
     <div className="expense-item">
       <div>
-        {date.toISOString()}
+        <div>{formattedDate.month}</div>
+        <div>{formattedDate.day}</div>
+        <div>{formattedDate.year}</div>
       </div>
       <div className="expense-item__description">
         <h2>{title}</h2>
