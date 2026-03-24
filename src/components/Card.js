77305c34ed6dd488821754b66411ead5b861a765
Alex Brilevski <brilevski.alex@gmail.com>
2026-03-24 21:05:52 +0300

    The wrapper component Card was added

---
+++ src/components/Card.js
@@ -0,0 +1,11 @@
+import './Card.css';
+
+function Card({ className, children }) {
+  const cssClasses = 'card' + (className ? (' ' + className) : '');
+
+  return (
+    <div className={cssClasses}>{children}</div>
+  );
+}
+
+export default Card;
