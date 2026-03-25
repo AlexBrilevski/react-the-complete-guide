ece64558a38fcd817b9477b19f8373f0ab1da364
Alex Brilevski <brilevski.alex@gmail.com>
2026-03-25 21:19:34 +0300

    React import statement was added to all files that use JSX

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import Expenses from "./components/Expenses";
 
 const expenses = [
@@ -28,6 +29,15 @@ const expenses = [
 ];
 
 function App() {
+  // return (
+  //   React.createElement(
+  //     'div', 
+  //     {}, 
+  //     React.createElement('h2', {}, 'Let\'s get started!'), 
+  //     React.createElement(Expenses, {items: expenses})
+  //   )
+  // );
+
   return (
     <div>
       <h2>Let's get started!</h2>
