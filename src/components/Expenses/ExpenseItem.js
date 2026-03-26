da5e48b1f634aaa059f59e58ceecc7ec67ee497b
Alex Brilevski <brilevski.alex@gmail.com>
2026-03-26 22:27:00 +0300

    Basic logic of updating an expense title was added

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
-const ExpenseItem = ({ date, title, amount }) => {
+const ExpenseItem = ({ date, title: propsTitle, amount }) => {
+  const [title, setTitle] = useState(propsTitle);
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
+        <button onClick={() => setTitle('Updated')}>Change title</button>
         <div className="expense-item__price">
           ${amount}
         </div>
