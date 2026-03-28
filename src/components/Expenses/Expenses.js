5454f494d26892ca23bbfb2f4bb651597e503942
Alex Brilevski <brilevski.alex@gmail.com>
2026-03-28 11:33:12 +0300

    Basic logic of selecting a year in the filter component was added

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import ExpenseItem from "./ExpenseItem";
 import './Expenses.css';
 
 const Expenses = ({items}) => {
+  const [selectedYear, setSelectedYear] = useState('');
+
+  const onSelectYear = (year) => {
+    setSelectedYear(year);
+  };
+
   return (
     <Card className="expenses">
-      <ExpensesFilter />
+      <ExpensesFilter onFilter={onSelectYear}/>
       {items.map(item => (
         <ExpenseItem key={item.id} {...item} />
       ))}
