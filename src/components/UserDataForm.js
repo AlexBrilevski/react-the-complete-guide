d664931c970a95818879e2ae73f6de3e8e7ff8ee
Alex Brilevski <brilevski.alex@gmail.com>
2026-04-03 22:43:03 +0300

    User data form markup and style was implemented

--- src/components/UserDataForm.js
+++ src/components/UserDataForm.js
@@ -1,6 +1,22 @@
+import classes from './UserDataFrom.module.css';
+
 const UserDataForm = () => {
   return (
-    <form></form>
+    <form className={classes.userDataForm}>
+      <div className={classes.formControls}>
+        <div className={classes.formControl}>
+          <label htmlFor="name">Name</label>
+          <input id="name" />
+        </div>
+        <div className={classes.formControl}>
+          <label htmlFor="age">Age</label>
+          <input id="age" />
+        </div>
+      </div>
+      <div className={classes.formActions}>
+        <button>Add</button>
+      </div>
+    </form>
   );
 };
 
